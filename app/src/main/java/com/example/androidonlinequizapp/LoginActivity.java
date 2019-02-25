9ef448b76d1a4d4b07df19f0b5110be1a3506f5a
Erik Strand <erikstrand00@gmail.com>
2019-02-25 15:14:50 +0100

    Added googel sign in option

--- app/src/main/java/com/example/androidonlinequizapp/LoginActivity.java
+++ app/src/main/java/com/example/androidonlinequizapp/LoginActivity.java
@@ -9,20 +9,32 @@ import android.widget.Toast;
 
 import com.example.androidonlinequizapp.Common.Common;
 import com.example.androidonlinequizapp.Model.User;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.common.SignInButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 
 import static com.example.androidonlinequizapp.R.layout.activity_login;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int RC_SIGN_IN = 123;
 
     EditText mUsername, mPassword;
     Button mSignInButton;
+
+    SignInButton mGoogleSignInButton;
 
     FirebaseDatabase database;
     DatabaseReference users;
@@ -36,6 +48,8 @@ public class LoginActivity extends AppCompatActivity {
         mUsername = findViewById(R.id.username);
         mPassword = findViewById(R.id.password);
         mSignInButton = findViewById(R.id.username_sign_in_button);
+        mGoogleSignInButton = findViewById(R.id.google_sign_in_button);
+        mGoogleSignInButton.setOnClickListener(this);
 
         //Firebase
         database = FirebaseDatabase.getInstance();
@@ -81,4 +95,48 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
+
+    public void createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+        // [END auth_fui_create_intent]
+    }
+
+
+    // [START auth_fui_result]
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        createSignInIntent();
+    }
+    // [END auth_fui_result]
 }
