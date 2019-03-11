23f55416c3177605abd3230b464e1b86eeb9c6d6
ster1666 <erikstrand00@student.ju.se>
2019-03-11 11:33:37 +0100

    minor changes

--- app/src/main/java/com/example/androidonlinequizapp/SettingsFragment.java
+++ app/src/main/java/com/example/androidonlinequizapp/SettingsFragment.java
@@ -10,10 +10,16 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import static com.google.firebase.inappmessaging.internal.Logging.TAG;
 
 
 public class SettingsFragment extends Fragment {
@@ -21,9 +27,6 @@ public class SettingsFragment extends Fragment {
     View myFragment;
 
     Button btnChangePwd,btnSignOut;
-
-    RecyclerView settingsView;
-    LinearLayoutManager layoutManager;
 
     public static SettingsFragment newInstance()
     {
@@ -46,16 +49,30 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         myFragment = inflater.inflate(R.layout.fragment_settings, container, false);
 
-        settingsView = myFragment.findViewById(R.id.settingsView);
-        layoutManager = new LinearLayoutManager(getActivity());
-        settingsView.setHasFixedSize(true);
 
-        layoutManager.setReverseLayout(true);
-        layoutManager.setStackFromEnd(true);
-        settingsView.setLayoutManager(layoutManager);
-
-        btnSignOut = myFragment.findViewById(R.id.logoutButton);
+        btnSignOut = myFragment.findViewById(R.id.signoutButton);
         btnChangePwd = myFragment.findViewById(R.id.changePasswordButton);
+
+        btnChangePwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Signout button clicked");
+
+                FirebaseAuth.getInstance().signOut();
+
+
+
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return myFragment;
     }
